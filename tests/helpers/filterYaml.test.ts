ba4788ef9009570ff326b50874801d01a3336437
Aiden Payne <aiden@thepaynes.com.au>
2023-07-26 13:42:18 +1000

    Added unit testing for the filterYaml function

---
+++ tests/helpers/filterYaml.test.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import YAML from "yaml";
+import filterYaml from "../../src/helpers/filterYaml";
+import { LANGUAGES } from "../../src/types/languages";
+import { type } from "../../src/types/entity";
+
+describe("Filter YAML", () => {
+  let mock: type;
+  let filteredMock: type;
+  beforeAll(() => {
+    mock = YAML.parse(fs.readFileSync(`${__dirname}/mocks/caldariShuttle.yaml`, "utf-8")) as type;
+    filteredMock = YAML.parse(fs.readFileSync(`${__dirname}/mocks/caldariShuttleFiltered.yaml`, "utf-8")) as type;
+  });
+
+  it("should filter languages based on the given language code. e.g. en", async () => {
+    const langKeys = [LANGUAGES.en];
+    const response = filterYaml(mock, langKeys);
+
+    expect(response).toEqual(filteredMock);
+  });
+});
